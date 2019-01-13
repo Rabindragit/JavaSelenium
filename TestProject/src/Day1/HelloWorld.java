d0f94da39c305ed9222b0c82ff52e1be479a5ad4
rabindra <ravinarayanmondal@gmail.com>
2019-01-13 22:38:37 +0530

    my java selenium practice

---
+++ TestProject/src/Day1/HelloWorld.java
@@ -0,0 +1,25 @@
+package Day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+	WebDriver driver =new FirefoxDriver();
+		 
+    driver.get("https://www.facebook.com");
+    driver.findElement(By.xpath("//input[@type='submit']")).click();    		
+    
+   System.out.println("Page loaded sucessfully");
+   
+   System.out.println(driver.getTitle());
+   System.out.println(driver.getCurrentUrl());
+
+   driver.close();
+	
+	}
+
+}
---
+++ TestProject/src/Day1/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author rabindra
+ *
+ */
+package Day1;
